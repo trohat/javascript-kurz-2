f7c677b11463745d5cafc501e909d165943f9224
Trohat <petr.sed@centrum.cz>
2023-08-03 20:14:20 +0200

    vstup a výstup

---
+++ 08-03 vstup a výstup/script.js
@@ -0,0 +1,31 @@
+console.log("funguju");
+
+console.log(typeof (5 + 7));
+
+let pozdrav = "ahoj";
+
+console.log(pozdrav);
+
+console.log(typeof(pozdrav))
+
+console.log(pozdrav + " Petře");
+
+console.warn("varuju tě!!!");
+
+console.error("Vznikla chyba");
+
+// chci si poznamenat něco pro sebe
+
+// TODO: dodělat kalkulačku!!
+// nejpozději zítra
+
+/* ahoj jak se máš
+půjdem do kina?
+jasně */
+
+// poznamenám si sem tajné heslo
+// ke svým penězům
+// je to SLON
+
+let name = "Petr";
+console.log(name);
---
+++ 08-03 vstup a výstup/script2.js
@@ -0,0 +1,33 @@
+console.log("jsme ve scriptu 2");
+
+// alert("AHOJ");
+
+// alert("Fakt chceš vidět ještě jeden alert box? Seš opilej nebo co?");
+
+// confirm("Máš dneska dobrou náladu?");
+
+// let pocetPiv = prompt("Kolik jsi dneska vypil piv?");
+
+// KAM = CO
+
+// console.log("Dneska jsi vypil " + pocetPiv + " piv.");
+
+alert("Jdeme sčítat čísla...");
+
+let cislo1;
+
+cislo1 = prompt("Zadej první cislo...");
+
+cislo1 = Number(cislo1);
+
+let cislo2 = prompt("Zadej druhé číslo...");
+
+cislo2 = Number(cislo2);
+
+let vysledek = cislo1 + cislo2;
+
+console.log("Výsledek je ... " + vysledek);
+
+// console.log(vysledek);
+
+
