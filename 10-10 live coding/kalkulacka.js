f15f8cef1123b6f233bc0807c4ead653114dff35
Trohat <petr.sed@centrum.cz>
2023-10-10 20:27:24 +0200

    Live coding 4

--- 10-10 live coding/kalkulacka.js
+++ 10-10 live coding/kalkulacka.js
@@ -1,9 +1,63 @@
+function vypocitej(event) {
+    const priklad = event.currentTarget.closest(".priklad");
+    const prvniInput = priklad.querySelector(".prvni");
+    const select = priklad.querySelector(".znamenko");
+    const druhyInput = priklad.querySelector(".druhe");
+    const divProVysledek = priklad.querySelector(".vysledek");
+    const cislo1 = Number(prvniInput.value);
+    if (Number.isNaN(cislo1)) {
+        return;
+    }
+    const cislo2 = Number(druhyInput.value);
+    if (Number.isNaN(cislo2)) {
+        return;
+    }
+    const operace = select.value;
+    let vysledek;
+    if (operace === "plus") {
+        vysledek = cislo1 + cislo2;
+    } else if (operace === "minus") {
+        vysledek = cislo1 - cislo2;
+    } else if (operace === "krat") {
+        vysledek = cislo1 * cislo2;
+    } else if (operace === "deleno") {
+        if (cislo2 === 0) {
+            divProVysledek.textContent = "";
+            return;
+        }
+        vysledek = cislo1 / cislo2;
+    } else {
+        return;
+    }
+    divProVysledek.textContent = vysledek;
+}
+
 function vyrobInput(jmeno) {
     const input = document.createElement("input");
     input.setAttribute("type", "number");
     input.setAttribute("name", jmeno);
     input.classList.add(jmeno);
+    input.addEventListener("input", vypocitej);
     return input;
+}
+
+function vyrobOption(hodnota, text) {
+    const option = document.createElement("option");
+    option.value = hodnota;
+    option.textContent = text;
+    return option;
+}
+
+function vyrobSelect() {
+    const vybiratko = document.createElement("select");
+    vybiratko.name = "znamenko";
+    vybiratko.classList.add("znamenko");
+    vybiratko.append(vyrobOption("plus", "+"));
+    vybiratko.append(vyrobOption("minus", "-"));
+    vybiratko.append(vyrobOption("krat", "*"));
+    vybiratko.append(vyrobOption("deleno", "/"));
+    vybiratko.addEventListener("input", vypocitej);
+    return vybiratko;
 }
 
 function vyrobDiv(trida, textUvnitr) {
@@ -17,16 +71,21 @@ function vyrobTlacitko() {
     const tlacitko = document.createElement("button");
     tlacitko.classList.add("vypocitej");
     tlacitko.textContent = "Vypočítej";
+    tlacitko.addEventListener("click", vypocitej)
     return tlacitko;
 }
 
 function pridejPriklad() {
     const priklad = document.createElement("section");
     priklad.classList.add("priklad");
-    // vytvoř prvky
+    
+    // tvoříme prvky
     const prvniInput = vyrobInput("prvni");
     priklad.append(prvniInput);
-// nezapomeň přidat SELECT
+    
+    const select = vyrobSelect();
+    priklad.append(select);
+
     const druhyInput = vyrobInput("druhe");
     priklad.append(druhyInput);
 
