715db52bf5184fd3f1b7e8931cae068597404b09
Trohat <petr.sed@centrum.cz>
2023-10-10 19:18:27 +0200

    Live coding 3

--- 10-10 live coding/kalkulacka.js
+++ 10-10 live coding/kalkulacka.js
@@ -6,7 +6,19 @@ function vyrobInput(jmeno) {
     return input;
 }
 
+function vyrobDiv(trida, textUvnitr) {
+    const div = document.createElement("div");
+    div.classList.add(trida);
+    div.textContent = textUvnitr;
+    return div;
+}
 
+function vyrobTlacitko() {
+    const tlacitko = document.createElement("button");
+    tlacitko.classList.add("vypocitej");
+    tlacitko.textContent = "Vypočítej";
+    return tlacitko;
+}
 
 function pridejPriklad() {
     const priklad = document.createElement("section");
@@ -17,6 +29,17 @@ function pridejPriklad() {
 // nezapomeň přidat SELECT
     const druhyInput = vyrobInput("druhe");
     priklad.append(druhyInput);
+
+    // delší způsob, s pomocnou proměnnou
+    const rovnaSeDiv = vyrobDiv("rovnase", "=");
+    priklad.append(rovnaSeDiv);
+
+    // kratší způsob, bez použití proměnné
+    priklad.append(vyrobDiv("vysledek", ""));
+
+    // opět delší způsob ... ono se to totiž lépe čte
+    const tlacitko = vyrobTlacitko();
+    priklad.append(tlacitko);
 
     const main = document.querySelector("main");
     main.append(priklad);
